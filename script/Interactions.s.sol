5a9e5896e1273f9395db7a4d48dfed2d00ada264
azeezokhamena <azeezokhamena@gmail.com>
2025-05-15 05:26:39 +0100

    Add NFT minting functionality and update configuration for permissions

---
+++ script/Interactions.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^ 0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract Interactions is Script {
+    string public constant DOG_TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    function run() external {
+        vm.startBroadcast();
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+
+    function mintNftOnContract(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        BasicNft(mostRecentlyDeployed).mintNft(DOG_TOKEN_URI);
+        vm.stopBroadcast();
+    }
+}
--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -9,6 +9,10 @@ import {BasicNft} from "../src/BasicNft.sol";
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    string public constant DOG_TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    address public USER = makeAddr("USER");
 
     function setUp() external {
         deployer = new DeployBasicNft();
@@ -19,5 +23,19 @@ contract BasicNftTest is Test {
         string memory name = basicNft.name();
         string memory expectedName = "Doggie";
         assert(keccak256(abi.encodePacked(name)) == keccak256(abi.encodePacked(expectedName)));
+    }
+
+    function testSymbolIsCorrect() external view {
+        string memory symbol = basicNft.symbol();
+        string memory expectedSymbol = "DOG";
+        assert(keccak256(abi.encodePacked(symbol)) == keccak256(abi.encodePacked(expectedSymbol)));
+    }
+
+    function testIfUserCanMintNft() external {
+        vm.prank(USER);
+        basicNft.mintNft(DOG_TOKEN_URI);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(basicNft.tokenURI(0))) == keccak256(abi.encodePacked(DOG_TOKEN_URI)));
     }
 }
